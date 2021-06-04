014d13d6dd1d6400c8f1bad6fa34929961084429
thiagomoralles <thiagomoralles@outlook.com>
2021-06-03 23:35:42 -0300

    Feign Clients

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -11,21 +11,20 @@ import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	RestTemplate restTemplate;
+	WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+//		Map<String, String> uriVariables = new HashMap<>();
+//		uriVariables.put("id", String.valueOf(workerId));
+//		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
