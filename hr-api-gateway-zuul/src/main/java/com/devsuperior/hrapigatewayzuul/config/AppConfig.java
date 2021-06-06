39b591096b9147f944f1046417fd7b7c998208a7
thiagomoralles <thiagomoralles@outlook.com>
2021-06-05 22:34:52 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.devsuperior.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+	// @Value("${jwt.secret}")
+	private String jwtSecret = "MY-SECRET-KEY";
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey(jwtSecret);
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+}
